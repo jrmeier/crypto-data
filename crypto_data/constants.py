b1a8b5583db4744edb037f187b8d715fbd35d92c
Jed Meier <jed@gaincompliance.com>
2019-12-05 11:59:14 -0600

    conversion

--- crypto_data/constants.py
+++ crypto_data/constants.py
@@ -12,6 +12,15 @@ TRADING_FILE_CSV_HEADER = [
     "close_time",
 ]
 
+NEW_TRADING_FILE_CSV_HEADER = [
+    "date",
+    "last_price",
+    "open_price",
+    "high_price",
+    "low_price",
+    "volume",
+]
+
 BINANCE_HEADER_MATCHING = {
     "price_change": "priceChange",
     "weighted_avg_price": "weightedAvgPrice",
@@ -25,6 +34,18 @@ BINANCE_HEADER_MATCHING = {
     "open_time": "openTime",
     "close_time": "closeTime",
 }
+
+OLD_2018_HEADER_MATCHING = {
+    "last_price": 5,
+    "open_price": 2,
+    "high_price": 3,
+    "low_price": 4,
+    "volume": 6,
+}
+
+# ,Date,Open,High,Low,Close,Volume,price_change,price_change_percent,weighted_avg_price,prev_close_price,last_qty,bid_price,ask_price
+#0,2018-01-01 01:48:01,12970.58,14050.11,12425.98,13380.6,11196,410.02,3.161,13272.2939873,12970.58,0,13380.6,13401.09
+
 
 AVALIABLE_MARKETS = [
     "BINANCE"
